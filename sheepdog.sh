a9f3a3c522751c3e3a3f7089b730ff71f34e65b7
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 11:31:46 -0600

    All quiet again

--- sheepdog.sh
+++ sheepdog.sh
@@ -2,7 +2,7 @@
 
 echo " "
 echo " "
-echo "Starting Sheepdog v9.2L"
+echo "Starting Sheepdog v9.3"
 echo " "
 echo " "
 
@@ -18,11 +18,11 @@ TAIL_PASSWORD="2GintzUR30SrRBmzvFYxjdn9wMuI1VrmFICFz1NV"
 
 echo "Starting downloads"
 
-sudo wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
+sudo wget -q -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 
 echo "Collected new sheepit"
 
-sudo wget --no-check-certificate --content-disposition --no-cache https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh -O launch.sh
+sudo wget -q --no-check-certificate --content-disposition --no-cache https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh -O launch.sh
 
 echo "Collected new launcher"
 
@@ -31,4 +31,4 @@ echo "Logging in as: $TAIL_USERNAME"
 sudo tmux new-session -d -s GPU 'java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"'
 sudo tmux new-session -d -s CPU 'java -jar sheepit.jar -ui text -compute-method CPU -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"'
 
-echo "Sheepdog Out"
+echo "Sheetm Out"
