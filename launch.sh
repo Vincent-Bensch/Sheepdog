c77ac3f3a9e9057bea9ef9b396dea7193cc60498
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 02:57:31 -0600

    remove tail

--- launch.sh
+++ launch.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 sudo rm -f sheepdog.sh
-sudo rm -f tail
 
 wget -O sheepdog.sh https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/sheepdog.sh
-wget -O tail https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/tail
 
 sudo bash sheepdog.sh
--- sheepdog.sh
+++ sheepdog.sh
@@ -4,16 +4,13 @@ source tail
 sudo rm -f sheepit.jar
 sudo rm -f launch.sh
 
-echo "---------------READING r5--------------------------"
-
-echo $TAIL_USERNAME
-echo $TAIL_PASSWORD
+TAIL_USERNAME = "vbensch"
+TAIL_PASSWORD = "2GintzUR30SrRBmzvFYxjdn9wMuI1VrmFICFz1NV"
 
 wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 wget -O launch.sh https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh
 
-echo "Logging in with username: $TAIL_USERNAME"
-echo "Logging in with password: $TAIL_PASSWORD"
+echo "Logging in as: $TAIL_USERNAME"
 
 sudo java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
 sudo java -jar sheepit.jar -ui text -compute-method CPU -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
