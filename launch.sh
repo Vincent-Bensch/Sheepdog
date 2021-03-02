03814a827d01b7753ed1368563133470dd7f1571
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 03:26:53 -0600

    I'm dying over here

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo wget --no-check-certificate --content-disposition --no-cache -q -O https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/sheepdog.sh
+sudo wget --no-check-certificate --content-disposition --no-cache -q  https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/sheepdog.sh -O sheepdog.sh
 
 sudo bash sheepdog.sh
