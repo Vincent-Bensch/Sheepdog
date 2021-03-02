1de1ce2fd29f129f692f76c04110af4d5710622c
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 03:16:11 -0600

    Update sheepdog.sh

--- sheepdog.sh
+++ sheepdog.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source tail
 
-echo "Starting Sheepdog v6"
+echo "Starting Sheepdog v7"
 
 sudo rm -f sheepit.jar
 sudo rm -f launch.sh
 
-TAIL_USERNAME = "vbensch"
-TAIL_PASSWORD = "2GintzUR30SrRBmzvFYxjdn9wMuI1VrmFICFz1NV"
+TAIL_USERNAME="vbensch"
+TAIL_PASSWORD="2GintzUR30SrRBmzvFYxjdn9wMuI1VrmFICFz1NV"
 
 sudo wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 sudo wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh
