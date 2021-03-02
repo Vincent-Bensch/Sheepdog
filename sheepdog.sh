5590def910560762d4e1964afb327673edf59e49
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 02:28:37 -0600

    Update sheepdog.sh

--- sheepdog.sh
+++ sheepdog.sh
@@ -5,6 +5,9 @@ rm sheepit.jar
 
 wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 
+echo "Logging in with username $TAIL_USERNAME"
+echo "Logging in with password $TAIL_PASSWORD"
+
 sudo java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
 sudo java -jar sheepit.jar -ui text -compute-method CPU -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
 
