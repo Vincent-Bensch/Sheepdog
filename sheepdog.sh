efaf5b39472cae5b19a1db87a4b5abe40e88b744
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 02:21:25 -0600

    Update sheepdog.sh

--- sheepdog.sh
+++ sheepdog.sh
@@ -2,7 +2,9 @@
 source tail
 
 rm sheepit.jar
+
 wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
+
 sudo java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -login "$USERNAME" -password "$PASSWORD"
 sudo java -jar sheepit.jar -ui text -compute-method CPU -login "$USERNAME" -password "$PASSWORD"
 
