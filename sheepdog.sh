ae4290cce8871cd3d892c4719c54dd0c6420e2eb
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 03:14:31 -0600

    Fixed now?

--- sheepdog.sh
+++ sheepdog.sh
@@ -9,8 +9,8 @@ sudo rm -f launch.sh
 TAIL_USERNAME = "vbensch"
 TAIL_PASSWORD = "2GintzUR30SrRBmzvFYxjdn9wMuI1VrmFICFz1NV"
 
-sudo wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php --no-cache
-sudo wget -O launch.sh https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh --no-cache
+sudo wget -O sheepit.jar https://www.sheepit-renderfarm.com/media/applet/client-latest.php
+sudo wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Vincent-Bensch/Sheepdog/master/launch.sh
 
 echo "Logging in as: $TAIL_USERNAME"
 
