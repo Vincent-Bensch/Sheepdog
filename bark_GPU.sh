1533d6bfac3d53e9b90e3e58186caaffcd6ce4db
Vincent Bensch <7649742+Vincent-Bensch@users.noreply.github.com>
2021-03-02 18:26:11 -0600

    Update bark_GPU.sh

--- bark_GPU.sh
+++ bark_GPU.sh
@@ -2,4 +2,4 @@
 
 source hydrant.sh
 
-java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -memory 6000000 -cores 1 -priority 15 -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
+sudo java -jar sheepit.jar -ui text -compute-method GPU -gpu CUDA_0 -memory 6000000 -cores 1 -priority 15 -login "$TAIL_USERNAME" -password "$TAIL_PASSWORD"
